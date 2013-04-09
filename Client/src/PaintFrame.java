8093dc9ff20566484a2692ccc94b61fa9db5c732
hypercross <helios.tnak@gmail.com>
2013-04-09 19:02:41 -0400

    upload eclipse project setup & clean up for a bit

---
+++ Client/src/PaintFrame.java
@@ -0,0 +1,53 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class PaintFrame extends JFrame {
+	
+	Socket clientSocket;
+	DataOutputStream out;
+	DataInputStream in;
+	
+	
+	public PaintFrame(String host, int port){
+		
+		setVisible(true);
+		setSize(500, 500);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.X_AXIS));
+		
+		try{
+			clientSocket = new Socket(host, port);  
+	        out = new DataOutputStream(clientSocket.getOutputStream());
+	        in = new DataInputStream(clientSocket.getInputStream());
+		}
+		catch(IOException ioe){
+			System.out.println("IOException setting up applet in frame...");
+		}
+		
+		getContentPane().add(new SidePanel(in, out));
+		getContentPane().add(new PaintApplet(in, out));
+	}
+	
+	
+	public static void main(String[] args){
+		if(args.length != 2)printUsageAndQuit();
+		
+		String host = args[0];
+		int port = Integer.parseInt(args[1]); 
+		
+		new PaintFrame(host, port);
+	}
+	
+	private static void printUsageAndQuit()
+	{
+		System.out.println("Usage: PaintFrame <host> <port>");
+		System.exit(1);
+	}
+	
+}
